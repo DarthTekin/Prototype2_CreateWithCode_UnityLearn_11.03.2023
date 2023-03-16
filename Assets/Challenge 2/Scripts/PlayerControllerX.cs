109c98acb4e45c53c81fe64225aaf148d309a1a5
DarthTekin <tekinteks@gmail.com>
2023-03-16 23:37:28 +0300

    Prototype2 scene uploaded; PlayerController.cs updated for vertical movements of the player.

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private bool canDog = true;
+
 
     // Update is called once per frame
     void Update()
+    {
+        SpawnDog();
+    }
+    void SpawnDog()
     {
         // On spacebar press, send dog
         if (Input.GetKeyDown(KeyCode.Space))
